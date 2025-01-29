15d8409cf111d068667901b51aabdf8d3a910b17
Justus Bachmann <40598558+JustusBachmann@users.noreply.github.com>
2025-01-29 14:08:28 +0100

    Update display.ino

--- display.ino
+++ display.ino
@@ -17,17 +17,14 @@ void draw(void (*renderFunction)()) {
   } while (u8g2.nextPage());
 }
 
+
 void renderLoadingScreen() {
   u8g2.setFont(u8g2_font_ncenB08_tr);
-
-  u8g2.drawCircle(64, 32, 20); // Centered circle for the logo
-  
-  u8g2.drawLine(64, 27, 64, 37); // Vertical line
-  u8g2.drawLine(59, 32, 69, 32); // Horizontal line
-
-  u8g2.setCursor(40, 55); // Adjust position as needed
+  u8g2.drawCircle(64, 32, 20);
+  u8g2.drawLine(64, 27, 64, 37);
+  u8g2.drawLine(59, 32, 69, 32);
+  u8g2.setCursor(40, 55);
   u8g2.print("Arduino");
-
   u8g2.setCursor(40, 10);
   u8g2.print("Loading...");
 }
